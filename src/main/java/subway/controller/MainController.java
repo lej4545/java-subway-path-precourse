f4d7ec85bb15228f941f07514daf2589008e354e
LEE EUN JIN <lej4545@naver.com>
2021-12-17 20:10:06 +0900

    feat(MainController): 지하철역 초기화 기능 구현
    지하철역 초기화 기능 구현
    - MainController
     - init(): 지하철역, 노선 setting

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,8 +1,43 @@
 package subway.controller;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
 public class MainController {
 
+    StationRepository stationRepository = new StationRepository();
+    LineRepository lineRepository = new LineRepository();
+    final String[] stations = new String[]{"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+    final String[] lines = new String[]{"2호선", "3호선", "신분당선"};
+
     public void run() {
-        System.out.println("프로그램 시작");
+        init();
+        printStation();
+        printLines();
     }
+
+    private void init() {
+        for (String station : stations) {
+            stationRepository.addStation(new Station(station));
+        }
+
+        for (String line : lines) {
+            lineRepository.addLine(new Line(line));
+        }
+    }
+
+    private void printStation() {
+        for (Station station : StationRepository.stations()) {
+            System.out.println(station.getName());
+        }
+    }
+
+    private void printLines() {
+        for (Line line : LineRepository.lines()) {
+            System.out.println(line.getName());
+        }
+    }
+
 }
